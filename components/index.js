57663b366c8ba90545778da8cd966b5dae8c5335
Antyibvak Ibrahim <tvax@Antyibvaks-MacBook-Pro.local>
2022-07-25 17:34:45 -0400

    random

--- components/index.js
+++ components/index.js
@@ -11,4 +11,6 @@ export {default as CommentsForm} from "./CommentsForm";
 export {default as Intro} from "./Intro";
 export {default as Loader} from "./Loader";
 export {default as Contact} from "./Contact";
+export {default as FeaturedPosts} from "./FeaturedPosts";
+
 
